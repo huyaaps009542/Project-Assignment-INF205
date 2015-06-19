3cbaaf13ebe89d66be4927812ed4cde52e053e0e
huyaaps009542 <huyaapsweb2052@gmail.com>
2015-06-19 17:26:52 +0700

    fix
    fix = tk 1 huyaaps00954

--- Application.Designer.vb
+++ Application.Designer.vb
@@ -14,10 +14,7 @@ Option Explicit On
 
 Namespace My
     
-    'NOTE: This file is auto-generated; do not modify it directly.  To make changes,
-    ' or if you encounter build errors in this file, go to the Project Designer
-    ' (go to Project Properties or double-click the My Project node in
-    ' Solution Explorer), and make changes on the Application tab.
+    'HUYAAPS00954 - ÂU ANH HUY tk1 fix on the this code........
     '
     Partial Friend Class MyApplication
         
